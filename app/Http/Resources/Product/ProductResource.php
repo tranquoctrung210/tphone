a97282caf3726e19e6673601f4193dcbab13f0f4
Tran Trung <quoctrung.tran210@gmail.com>
2021-11-10 18:25:36 +0700

    modified Product details

--- app/Http/Resources/Product/ProductResource.php
+++ app/Http/Resources/Product/ProductResource.php
@@ -18,8 +18,13 @@ class ProductResource extends JsonResource
             'name' => $this->name,
             'description' => $this->description,
             'price' => $this->price,
-            'stock' => $this->stock,
+            'totalPrice' => round(((1 - ($this->discount / 100)) * $this->price), 2),
+            'stock' => $this->stock > 0 ? $this->stock : 'Out of stock',
             'discount' => $this->discount,
+            'rating' => $this->reviews->count() > 0 ?  round($this->reviews->sum('star') / $this->reviews->count(), 2) : 'No rating yet',
+            'href' => [
+                'reviews' => route('reviews.index',  $this->id),
+            ]
         ];
     }
 }
