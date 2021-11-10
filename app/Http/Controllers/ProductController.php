b25f0a145c0016c919c812471ccd4ef28083cd67
Tran Trung <quoctrung.tran210@gmail.com>
2021-11-11 00:16:52 +0700

    product resource

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Product;
 use App\Http\Resources\Product\ProductResource;
+use App\Http\Resources\Product\DetailProductResource;
 use Illuminate\Http\Request;
 
 class ProductController extends Controller
@@ -15,7 +16,7 @@ class ProductController extends Controller
      */
     public function index()
     {
-        return Product::all();
+        return ProductResource::collection(Product::all());
     }
 
     /**
@@ -47,7 +48,7 @@ class ProductController extends Controller
      */
     public function show(Product $product)
     {
-        return new ProductResource($product);
+        return new DetailProductResource($product);
     }
 
     /**
--- app/Http/Resources/Product/DetailProductResource.php
+++ app/Http/Resources/Product/DetailProductResource.php
@@ -4,7 +4,7 @@ namespace App\Http\Resources\Product;
 
 use Illuminate\Http\Resources\Json\JsonResource;
 
-class ProductResource extends JsonResource
+class DetailProductResource extends JsonResource
 {
     /**
      * Transform the resource into an array.
@@ -16,11 +16,14 @@ class ProductResource extends JsonResource
     {
         return [
             'name' => $this->name,
+            'description' => $this->description,
+            'price' => $this->price,
             'totalPrice' => round(((1 - ($this->discount / 100)) * $this->price), 2),
             'stock' => $this->stock > 0 ? $this->stock : 'Out of stock',
+            'discount' => $this->discount,
             'rating' => $this->reviews->count() > 0 ?  round($this->reviews->sum('star') / $this->reviews->count(), 2) : 'No rating yet',
             'href' => [
-                'link' => route('products.show',  $this->id),
+                'reviews' => route('reviews.index',  $this->id),
             ]
         ];
     }
